a350a2c09de839ef5504e491589d48d33d7e442f
BenniKenobi <Bennedict.Schweimer@haw-hamburg.de>
2022-12-29 15:36:32 +0100

    BackgroundSubstraction

--- AnimalSingdom/Python/IdentifyAnimal.py
+++ AnimalSingdom/Python/IdentifyAnimal.py
@@ -10,23 +10,25 @@ pixel = [55,49,100]
 hp = 176
 sp = 130
 
-elephant = [133,136,130] #rgba(130,136,133,255)
-elephantH = 75
-elephantS = 11
+elephant = [112,122,120] #rgba(120,122,112,255)
+elephantH = 36
+elephantS = 20
 
-lion = [106,149,158] #rgba(158,149,106,255)
+lion = [102,177,196] #rgba(196,177,102,255)
 lionH = 24
-lionS = 83
+lionS = 122
 
-pig = [200,223,255] #rgba(255,223,200,255)
-pigH = 12
-pigS = 55
+pig = [168,183,192] #rgba(192,183,168,255)
+pigH = 18
+pigS = 31
 
-cat = [13,12,16] #rgba(16,12,13,255)
-catH = 172
-catS = 63
+cat = [46,44,44] #rgba(44,44,46,255)
+catH = 120
+catS = 11
 
 threshold = 5
+
+backSub = cv2.createBackgroundSubtractorKNN()
 
 def nothing(x):
     pass
@@ -44,6 +46,11 @@ def lookForElephant(frame):
     mask = huemask
     cv2.bitwise_and(huemask,satmask,mask)
 
+    if(np.sum(mask == 255) > 2000):
+        controller = 1
+        value = 2
+        sendMessage(controller,value)
+
     return mask
 
 def lookForLion(frame):
@@ -56,9 +63,47 @@ def lookForLion(frame):
     cv2.bitwise_and(huemask,satmask,mask)
 
 
-    if(np.sum(mask == 255) > 1000):
+    if(np.sum(mask == 255) > 2000):
         controller = 1
         value = 3
+        sendMessage(controller,value)
+    
+    
+
+    return mask
+
+def lookForPig(frame):
+    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    h,s,v = cv2.split(hsv)
+    huemask = cv2.inRange(h, pigH - threshold, pigH + threshold)
+    satmask = cv2.inRange(s, pigS - threshold, pigS + threshold)
+
+    mask = huemask
+    cv2.bitwise_and(huemask,satmask,mask)
+
+
+    if(np.sum(mask == 255) > 2000):
+        controller = 1
+        value = 4
+        sendMessage(controller,value)
+    
+    
+
+    return mask
+
+def lookForCat(frame):
+    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    h,s,v = cv2.split(hsv)
+    huemask = cv2.inRange(h, catH - threshold, catH + threshold)
+    satmask = cv2.inRange(s, catS - threshold, catS + threshold)
+
+    mask = huemask
+    cv2.bitwise_and(huemask,satmask,mask)
+
+
+    if(np.sum(mask == 255) > 2000):
+        controller = 1
+        value = 1
         sendMessage(controller,value)
     
     
@@ -73,12 +118,20 @@ cv2.namedWindow(windowName)
 while cap.isOpened():
 
     ret, frame = cap.read()
+
+    fgmask = backSub.apply(frame)
+    colorfgmask = cv2.cvtColor(fgmask,cv2.COLOR_GRAY2BGR)
+
+    whitePixels = (colorfgmask > 0)
+    colorfgmask[whitePixels] = frame[whitePixels]
+
+    mask = frame
     
-    mask = lookForLion(frame)
+    mask = lookForCat(colorfgmask)
 
     median = cv2.medianBlur(mask,5)
 
-    cv2.imshow(windowName,median)
+    cv2.imshow(windowName,mask)
     cv2.imshow("LiveFootage",frame)
 
     if cv2.waitKey(25) != -1:
